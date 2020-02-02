999a5296a2078b5b3d91f2f55f1af5ccab177a30
lalawue <suchaaa@gmail.com>
2020-02-02 22:48:43 +0800

    check binaries dir first

--- run_app.bat
+++ run_app.bat
@@ -2,11 +2,15 @@
 Rem
 Rem luajit app launcher by lalawue
 
-Rem export system library and Lua library path
-set PATH=%CD%\binaries\WindowsNT
+if exist %CD%\binaries\WindowsNT (
+    Rem export system library and Lua library path
+    set PATH=%CD%\binaries\WindowsNT
 
-Rem export LuaJIT path
-set LUA_PATH=?.lua;middle\?.lua;
+    Rem export LuaJIT path
+    set LUA_PATH=?.lua;middle\?.lua;
 
-Rem luajit invoke
-luajit.exe app_launcher.lua %*
+    Rem luajit invoke
+    luajit.exe app_launcher.lua %*
+) else (
+    echo [ERROR] binaries dir not exist, please build required binaries first !
+)
--- run_app.sh
+++ run_app.sh
@@ -2,13 +2,21 @@
 #
 # luajit app launcher by lalawue
 
+BINARIES_DIR=$PWD/binaries/$(uname)
+if [ ! -d $BINARIES_DIR ]; then
+    echo "binaries dir not exist, please cd build/ && ./proj_build.sh first !"
+    exit 0
+else
+    echo $BINARIES_DIR
+fi
+
 # export system library and Lua library path
 if [ "$(uname)" = "Darwin" ]; then
-    export DYLD_LIBRARY_PATH=$PWD/binaries/$(uname)
-    export LUA_CPATH=$PWD/binaries/$(uname)/lib?.dylib
+    export DYLD_LIBRARY_PATH=$BINARIES_DIR
+    export LUA_CPATH=$BINARIES_DIR/lib?.dylib
 else
-    export LD_LIBRARY_PATH=$PWD/binaries/$(uname)
-    export LUA_CPATH=$PWD/binaries/$(uname)/lib?.so
+    export LD_LIBRARY_PATH=$BINARIES_DIR
+    export LUA_CPATH=$BINARIES_DIR/lib?.so
 fi
 
 # export LuaJIT path
