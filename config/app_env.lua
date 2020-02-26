2569eea7c039082215fec2cbe43246a134155f84
lalawue <suchaaa@gmail.com>
2020-02-27 01:42:02 +0800

    update app_env for parse sproto spec at the very beginning

--- config/app_env.lua
+++ config/app_env.lua
@@ -1,9 +1,11 @@
--- 
+--
 -- Copyright (c) 2020 lalawue
--- 
+--
 -- This library is free software; you can redistribute it and/or modify it
 -- under the terms of the MIT license. See LICENSE for details.
 --
+
+local Sproto = require("middle.sproto")
 
 -- App environment
 local AppEnv = {}
@@ -12,28 +14,65 @@ local function _tmpDir()
     return jit.os == "Windows" and os.getenv("TMP") or "/tmp"
 end
 
+local function _parseSprotoSpec(file_path)
+    print("[AppEnv] parse sproto spec " .. file_path)
+    repeat
+        local f = io.open(file_path, "rb")
+        if not f then
+            break
+        end
+        local spec_content = f:read("*a")
+        f:close()
+        if not spec_content then
+            break
+        end
+        local bin_content = Sproto.parse(spec_content)
+        if not bin_content then
+            break
+        end
+        return bin_content
+    until false
+    print("[AppEnv] failed to parse sproto spec")
+    os.exit(0)
+end
+
 -- define your app config
 AppEnv.Config = {
-    APP_DIR = "",                -- user defined app dir
-    TMP_DIR = _tmpDir(),         -- tmp dir
-    DATA_DIR = _tmpDir() .. "/rpc_framework",
-    LOOP_IPV4 = '127.0.0.1',     -- loop ip
-    HOST_IPV4 = '127.0.0.1',     -- host ip
-    RPC_LOOP_CHECK = 8,          -- loop count to check timeout
-    BROWSER_TIMEOUT = 8,         -- not precise timeout
-    SPROTO_SPEC = "config/app_rpc_spec.sproto",
+    APP_DIR = "", -- app dir
+    TMP_DIR = _tmpDir(), -- tmp dir
+    DATA_DIR = _tmpDir() .. "/rpc_apps",
+    LOOP_IPV4 = "127.0.0.1", -- loop ip
+    HOST_IPV4 = "0.0.0.0", -- host ip
+    RPC_LOOP_CHECK = 8, -- loop count to check timeout
+    BROWSER_TIMEOUT = 8 -- not precise timeout
 }
 
 AppEnv.Prototols = {
+    TCP_RAW = "TCP_RAW", -- raw data in TCP, with 2 bytes length ahead
     HTTP_JSON = "HTTP_JSON", -- JSON in HTTP body
-    LUA_SPROTO = "SPROTO", -- https://github.com/cloudwu/sproto, like protobuf
+    LUA_SPROTO = "SPROTO" -- https://github.com/cloudwu/sproto, like protobuf, with 2 bytes length ahead
+}
+
+AppEnv.Store = {
+    -- parse sproto spec first
+    [AppEnv.Prototols.LUA_SPROTO] = _parseSprotoSpec("config/app_rpc_spec.sproto")
 }
 
 -- service info for publisher or caller
 AppEnv.Service = {
-   -- rpc_name = { name = "rpc_name", proto = 'AppEnv.Prototols', ipv4 = '127.0.0.1', port = 1024 }
-   DNS_JSON = { name = "dns_json", proto = AppEnv.Prototols.HTTP_JSON, ipv4 = AppEnv.Config.HOST_IPV4, port = 10053 },
-   DNS_SPROTO = { name = "dns_sproto", proto = AppEnv.Prototols.LUA_SPROTO, ipv4 = AppEnv.Config.LOOP_IPV4, port = 10054 },
+    -- rpc_name = { name = "rpc_name", proto = 'AppEnv.Prototols', ipv4 = '127.0.0.1', port = 1024 }
+    DNS_JSON = {
+        name = "dns_json",
+        proto = AppEnv.Prototols.HTTP_JSON,
+        ipv4 = AppEnv.Config.LOOP_IPV4,
+        port = 10053
+    },
+    DNS_SPROTO = {
+        name = "dns_sproto", -- message name in sproto spec
+        proto = AppEnv.Prototols.LUA_SPROTO,
+        ipv4 = AppEnv.Config.LOOP_IPV4,
+        port = 10054
+    }
 }
 
 -- set readonly mode
@@ -42,6 +81,7 @@ local err_message = "attempt to update AppEnv table"
 AppEnv.Config = table.readonly(AppEnv.Config, err_message)
 AppEnv.Prototols = table.readonly(AppEnv.Prototols, err_message)
 AppEnv.Service = table.readonly(AppEnv.Service, err_message)
+AppEnv.Store = table.readonly(AppEnv.Store, err_message)
 AppEnv = table.readonly(AppEnv, err_message)
 
 return AppEnv
