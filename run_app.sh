58711e0d4c5918f0ff1b9779f04d9c4a9e70da95
lalawue <suchaaa@gmail.com>
2020-03-15 13:14:34 +0800

    update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -2,25 +2,33 @@
 #
 # luajit app launcher by lalawue
 
-LOCAL_DIR=$PWD/binaries/
+LOCAL_DIR=$PWD/binaries/Local
 BINARIES_DIR=$PWD/binaries/$(uname)
+
+if [ "$#" == "0" ]; then
+    echo "Usage: $0 APP_NAME"
+    exit 0
+fi
 
 if [ ! -d $BINARIES_DIR ]; then
     echo "binaries dir not exist, please cd build/ && ./proj_build.sh first !"
     exit 0
 fi
 
-# export system library and Lua library path
+# LuaJIT path
+LUA_PATH="?.lua;middle/?.lua;"
+
+# system library and Lua library path
 if [ "$(uname)" = "Darwin" ]; then
-    export DYLD_LIBRARY_PATH=$BINARIES_DIR:$LOCAL_DIR/lib
-    export LUA_CPATH=$BINARIES_DIR/lib?.dylib
+    LIB_PATH=$BINARIES_DIR:$LOCAL_DIR/lib
+    LUA_CPATH=$BINARIES_DIR/lib?.dylib
+
+    # luajit invoke
+    env DYLD_LIBRARY_PATH=$LIB_PATH LUA_CPATH=$LUA_CPATH LUA_PATH=$LUA_PATH luajit app_launcher.lua $*    
 else
-    export LD_LIBRARY_PATH=$BINARIES_DIR:$LOCAL_DIR/lib:/usr/lib:/usr/local/lib
-    export LUA_CPATH=$BINARIES_DIR/lib?.so
+    LIB_PATH=$BINARIES_DIR:$LOCAL_DIR/lib:/usr/lib:/usr/local/lib
+    LUA_CPATH=$BINARIES_DIR/lib?.so
+
+    # luajit invoke
+    env LD_LIBRARY_PATH=$LIB_PATH LUA_CPATH=$LUA_CPATH LUA_PATH=$LUA_PATH luajit app_launcher.lua $*    
 fi
-
-# export LuaJIT path
-export LUA_PATH="?.lua;middle/?.lua;"
-
-# luajit invoke
-luajit app_launcher.lua $*
