4c0205ab9eb88766bd3d99cbbe10d7b71a04f321
lalawue <suchaaa@gmail.com>
2020-07-22 10:40:06 +0800

    update scratch library

--- base/scratch.lua
+++ base/scratch.lua
@@ -9,6 +9,9 @@
 
 local serpent = require("base.serpent")
 
+-- string
+--
+
 function string:split(sSeparator, nMax, bRegexp)
     assert(sSeparator ~= "")
     assert(nMax == nil or nMax >= 1)
@@ -32,6 +35,9 @@ function string:split(sSeparator, nMax, bRegexp)
     return aRecord
 end
 
+-- table
+--
+
 table.dump = function(tbl)
     print(serpent.block(tbl))
 end
@@ -49,6 +55,52 @@ table.readonly = function(tbl, err_message)
     )
 end
 
+local function _deep_copy(object, lookup_table)
+    if type(object) ~= "table" then
+        return object
+    elseif lookup_table[object] then
+        return lookup_table[object]
+    end
+    local new_object = {}
+    lookup_table[object] = new_object
+    for key, value in pairs(object) do
+        new_object[_deep_copy(key, lookup_table)] = _deep_copy(value, lookup_table)
+    end
+    return setmetatable(new_object, getmetatable(object))
+end
+
+if table.clone == nil then
+    function table.clone(o)
+        return _deep_copy(o, {})
+    end
+end
+
+-- is empty
+if table.isempty == nil then
+    function table.isempty(t)
+        return type(t) == "table" and _G.next(t) == nil
+    end
+end
+
+if table.isarray == nil then
+    function table.isarray(t)
+        if type(t) ~= "table" then
+            return false
+        end
+        local i = 0
+        for _ in pairs(t) do
+            i = i + 1
+            if t[i] == nil then
+                return false
+            end
+        end
+        return true
+    end
+end
+
+-- io
+--
+
 io.printf = function(fmt, ...)
     if not fmt then
         os.exit(0)
@@ -56,28 +108,28 @@ io.printf = function(fmt, ...)
     print(string.format(fmt, ...))
 end
 
+-- language
+--
+
 Lang = {}
 
-local function isAllType(name, ...)
-    for i = 1, 65536, 1 do
-        local input = select(i, ...)
-        if input == nil then
-            return i > 1
-        elseif type(input) ~= name then
+-- validate input parameter is valid type described in type_desc, only 1 depth
+local _type_string = {
+    ["I"] = "nil",
+    ["N"] = "number",
+    ["S"] = "string",
+    ["B"] = "boolean",
+    ["T"] = "table",
+    ["F"] = "function",
+    ["D"] = "thread",
+    ["U"] = "userdata"
+}
+function Lang.valid(type_desc, ...)
+    for i = 1, type_desc:len(), 1 do
+        local val = select(i, ...)
+        if type(val) ~= _type_string[type_desc:sub(i, i)] then
             return false
         end
     end
-    return false
-end
-
-function Lang.isString(...)
-    return isAllType("string", ...)
-end
-
-function Lang.isTable(...)
-    return isAllType("table", ...)
-end
-
-function Lang.isFunction(...)
-    return isAllType("function", ...)
+    return true
 end
